a5a33a8fab27a586606fadd74585f3fa0c37bf28
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-13 23:33:56 +0530

    Link Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route } from 'react-router-dom';
 
-import Welcome from './components/Welcome';
-import Products from './components/Products';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Route path="/welcome">
         <Welcome />
       </Route>
