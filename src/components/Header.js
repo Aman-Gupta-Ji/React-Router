5b0851a8ae8dc39f8ff40d613353b162731a17eb
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-14 00:31:23 +0530

    NavLink & Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to="/welcome">Welcome</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
