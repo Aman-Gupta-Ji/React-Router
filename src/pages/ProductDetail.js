e6f7dda96e8120149d5341640fd8d785ef8d8096
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-14 22:08:37 +0530

    Switch Routes & exact

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
-    return (
-        <section>
-            <h1>Product Detail Page</h1>
-        </section>
-    );
-}
+  const params = useParams();
+  console.log(params);
+  return (
+    <section>
+      <h1>Product Detail</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
+};
 
 export default ProductDetail;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Product Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>A Shelf</li>
+        <li>
+          <Link to="/products/book1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/carpet1">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/shelf1">A Shelf</Link>
+        </li>
       </ul>
     </section>
   );
