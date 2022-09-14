98555ccf34274e5460834dd8c670f59c682a556e
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-14 13:56:25 +0530

    Product Details /:Id

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import Header from './components/Header';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
-    </div>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/products-detail/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
+    </>
   );
 }
 
